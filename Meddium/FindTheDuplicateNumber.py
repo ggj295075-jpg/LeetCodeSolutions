06adc533db3c89f8ee66dbbb3a52e8d3c37eabf0
alo <ggj295075@gmail.com>
2026-09-08 22:00:17 +0400

    Added my first Meddium Problem solve from leetcode

---
+++ Meddium/FindTheDuplicateNumber.py
@@ -0,0 +1,12 @@
+# This is my solve therefor it not be the best.
+
+
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        map = {}
+        for i in range(len(nums)):
+            if f"{nums[i]}" in map:
+                return nums[i]
+            map[f"{nums[i]}"] = nums[i]
+
+        return 0
